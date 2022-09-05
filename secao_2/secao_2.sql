2f280cc4c6b0f3f2e3b9739ebe570e19b4b43aeb
Perkles <adrianegonaves@gmail.com>
2022-09-05 09:19:51 -0300

    Seção 2

---
+++ secao_2/secao_2.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE bancodedados;
+
+SHOW DATABASES;
+
+DROP DATABASE teste;
+DROP DATABASE usuarios;
+
+--- exercicio 
+
+CREATE DATABASE computador; 
+SHOW DATABASES;
+DROP DATABASE computador;
+
+USE  empresa;
+SELECT * FROM empresa.funcionarios;
